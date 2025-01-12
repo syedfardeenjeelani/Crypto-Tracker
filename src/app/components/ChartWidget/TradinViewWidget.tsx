0bc1040af5a20e4c267f128539ecf4f5669abb0b
syedfardeenjeelani <syedfardeenjeelani13@gmail.com>
2025-01-12 16:19:08 +0530

    fix: fixed css

--- src/app/components/ChartWidget/TradinViewWidget.tsx
+++ src/app/components/ChartWidget/TradinViewWidget.tsx
@@ -13,7 +13,7 @@ function TradingViewWidget() {
     script.innerHTML = `
         {
           "width": "100%",
-          "height": "650",
+          "height": "600",
             "symbol": "BITSTAMP:BTCUSD",
           "interval": "D",
           "timezone": "Etc/UTC",
